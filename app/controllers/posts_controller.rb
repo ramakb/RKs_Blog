1215eb3660ae9d0a579b9c26e3beb66baf34baa0
ramakb <rama_kb@hotmail.com>
2015-01-23 11:49:56 +0000

    Add new, edit, show for Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+  
+  def new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    
+    redirect_to @post
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+    def find_post
+    end
+  
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
